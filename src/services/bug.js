573558b2bd84cfcde453e902d3d6397b8fdae04c
Muhiddin Kurbonov <muhiddindc@gmail.com>
2023-04-29 17:19:09 -0500

    implement bugs, bug card, style it, add date format util

--- src/services/bug.js
+++ src/services/bug.js
@@ -2,14 +2,16 @@ import axios from "axios";
 import Cookies from "js-cookie";
 const API_BASE_URL = process.env.REACT_APP_API_ENDPOINT;
 
+
+ const token = Cookies.get("token");
+ const config = {
+   headers: {
+     Authorization: `Bearer ${token}`,
+     "Content-Type": "application/json",
+   },
+ };
+
 export const addBug = async (bug) => {
-    const token = Cookies.get("token");
-    const config = {
-      headers: {
-        Authorization: `Bearer ${token}`,
-        "Content-Type": "application/json",
-      }
-    };
   try {
     const response = await axios.post(`${API_BASE_URL}/bugs`, bug, config);
     return response.data;
@@ -17,3 +19,12 @@ export const addBug = async (bug) => {
     console.log(error.response.data.message);
   }
 };
+
+export const getBugs = async () => {
+  try {
+    const response = await axios.get(`${API_BASE_URL}/bugs`, config);
+    return response.data;
+  } catch (error) {
+    console.log(error.response.data.message);
+  }
+}
