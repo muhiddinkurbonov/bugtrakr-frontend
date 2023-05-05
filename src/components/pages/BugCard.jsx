7ce9d08611fc8a37c3fd9a8148f552a46fefa740
Muhiddin Kurbonov <muhiddindc@gmail.com>
2023-05-04 20:40:57 -0500

    add edit, delete icons and style them

--- src/components/pages/BugCard.jsx
+++ src/components/pages/BugCard.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { convertTSToLocal } from "../utils/date";
+import EditIcon from '../../svg/edit.svg'
+import DeleteIcon from "../../svg/del.svg";
 
 export const BugCard = ({ bug }) => {
   const date = convertTSToLocal(bug.createdAt);
   return (
     <div className="bug-card">
+      <div className="edit-del">
+        <img src={EditIcon} alt="" className="icon" />
+        <img src={DeleteIcon} alt="" className="icon" />
+      </div>
       <p className="bugTitle">
-        Title: <span className="title">{bug.title}</span>
+        <span className="title">{bug.title}</span>
       </p>
       <p>Description: {bug.description}</p>
       <p>Priority: {bug.priority}</p>
